5b738bdcc55f244755923f2bf9e6bd7d3baad0de
maronnjapan <tihoutaikai20@gmail.com>
2025-12-30 00:32:01 +0900

    tRPCのサンプルプロジェクト削除

--- apps/my-app/database/drizzle/db.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle as drizzleD1 } from "drizzle-orm/d1";
-
-export function dbD1(d1: D1Database) {
-  return drizzleD1(d1);
-}
--- apps/my-app/drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "sqlite",
-  schema: "./database/drizzle/schema/*",
-  out: "./database/migrations",
-});
